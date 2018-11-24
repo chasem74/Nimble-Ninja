a45eed367c5a5685236f8abf286b56949aa8caaf
Conrad Chase Metzger <chasemetzger15@gmail.com>
2018-11-24 03:34:40 -0800

    Changed the way I'm moving and rendering the MovingGround

--- Assets/Scripts/MovingGround.cs
+++ Assets/Scripts/MovingGround.cs
@@ -4,27 +4,28 @@ using UnityEngine;
 
 public class MovingGround : MonoBehaviour
 {
-	public GameObject platformPrefab;
-	public int numberOfSegments = 20;
-	// Start is called before the first frame update
+	static int initialNumberOfSegments = 20;
 
-	private GameObject[] platforms;
+	public GameObject platformPrefab;
+	public int numberOfSegments = initialNumberOfSegments;
+
+	GameObject[] platforms;
+
 	void Start()
 	{
 		platforms = new GameObject[numberOfSegments];
 		float width = platformPrefab.transform.localScale.x;
 		for (int i = 0; i < platforms.Length; ++i)
 		{
-			Vector3 pos = new Vector3((float)i * width, 0);
+			Vector3 pos = new Vector3(i * width, 0);
 			platforms[i] = Instantiate(platformPrefab, pos, Quaternion.identity);
 			platforms[i].transform.SetParent(transform);
-			platforms[i].GetComponent<SpriteRenderer>().color = (i % 2 == 0) ? Color.green : Color.gray;
+			platforms[i].GetComponent<SpriteRenderer>().color = new Color(i * 10.0f, 0, 0);//(i % 2 == 0) ? Color.green : Color.gray;
 		}
 	}
 
-    // Update is called once per frame
-    void Update()
+	void Update()
 	{
-		transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.left * 200, Time.deltaTime);
-    }
+		transform.Translate(Vector3.left * 100.0f * Time.deltaTime);
+	}
 }
